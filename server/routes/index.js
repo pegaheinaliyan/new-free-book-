076335bd322ac07357073ee45d84673e416255cf
pegaheinaliyan <pegah.eitl96@gmail.com>
2017-08-21 14:02:06 +0700

    adding knexfile and services

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
---
+++ services/db/index.js
@@ -0,0 +1,17 @@
+const Knex = require('knex');
+
+module.exports = function(config){
+  const knex = Knex({
+    client: config.client,
+    port     : config.connection.port,
+    connection: {
+      host     : config.connection.host,
+      database : config.connection.database
+    }
+  });
+
+  return {
+    stickers: require('./books')(knex),
+  };
+
+};
